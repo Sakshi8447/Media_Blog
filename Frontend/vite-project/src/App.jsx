1aaa91533300df174283dc3ded9a0413806288e0
Sakshi <sakshipathak3007@gmail.com>
2024-04-03 11:52:13 +0530

    redux functionlity added

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home.jsx"
+import {Header} from "./components/Header.jsx"
+import {Login} from "./pages/Login.jsx"
+import {Register } from "./pages/Register.jsx"
+import { useEffect , useState} from "react"
+import axios from "axios"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.post('http://localhost:8000/api/user/currentUser', { withCredentials: true });
+        console.log('User Data:', response.data);
+        setUser(response.data.data);
+        // Add any additional handling after API call
+      } catch (error) {
+        console.error('API Error:', error.response.data.message);
+        // Handle API error
+      }
+    }
+    fetchUser();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Header user = {user}/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ Frontend/vite-project/src/components/Header.jsx
@@ -0,0 +1,66 @@
+import { Navbar, Dropdown, Avatar } from "flowbite-react"
+import { useEffect, useState } from 'react'
+import {useSelector} from 'react-redux' // 1st line
+
+const Header = () => {
+    const user = useSelector(state => state.user.currentUser) // 2nd line
+    console.log(user);
+    const [isLoggedin, setIsLoggedin] = useState(false)
+
+    useEffect(() => {
+        if (user) {
+            setIsLoggedin(true)
+        }
+    }, [user])
+
+    const handleSignOut = () => {
+        // sign out api call
+        setIsLoggedin(false)
+    }
+
+    return (
+        <>
+            <Navbar fluid rounded>
+                <Navbar.Brand href="https://flowbite-react.com">
+                    <img src="public/Logo.png" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Latest Tech Blogs</span>
+                </Navbar.Brand>
+                <div className="flex md:order-2">
+                    {
+                        isLoggedin ?
+                            (<Dropdown
+                                arrowIcon={false}
+                                inline
+                                label={
+                                    <Avatar alt="User settings" img={user.avatar} rounded />
+                                }
+                            >
+                                <Dropdown.Header>
+                                    <span className="block text-sm">{user.username}</span>
+                                    <span className="block truncate text-sm font-medium">{user.email}</span>
+                                </Dropdown.Header>
+                                <Dropdown.Item>Dashboard</Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item onClick={handleSignOut}>Sign out</Dropdown.Item>
+                            </Dropdown>) :
+                            (
+                                <a href="/login" > Login </a>
+                            )
+                    }
+                    <Navbar.Toggle />
+                </div>
+                <Navbar.Collapse>
+                    <Navbar.Link href="#" active>
+                        Home
+                    </Navbar.Link>
+                    <Navbar.Link href="#">About</Navbar.Link>
+                    <Navbar.Link href="#">Services</Navbar.Link>
+                    <Navbar.Link href="#">Pricing</Navbar.Link>
+                    <Navbar.Link href="#">Contact</Navbar.Link>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    )
+}
+
+export { Header }
