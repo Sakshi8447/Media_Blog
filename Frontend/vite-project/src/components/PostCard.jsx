1d0997b4f0614ba9b1946977276af933c1939f01
Sakshi <sakshipathak3007@gmail.com>
2024-04-07 20:19:31 +0530

    Add postcards and make footer

--- Frontend/vite-project/src/components/PostCard.jsx
+++ Frontend/vite-project/src/components/PostCard.jsx
@@ -4,19 +4,17 @@ import { Card } from "flowbite-react"
 
 const PostCard = ({ post }) => {
     return (
-        <>
-            {/* <div>
-                <Card className="max-w-sm" imgSrc={post.image} horizontal>
-                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        {post.title}
-                    </h5>
-                    <p className="font-normal text-gray-700 dark:text-gray-400">
-                        {post.content.slice(0, 100)}
-                    </p>
-                </Card>
-            </div> */}
-        </>
-    )
-}
+        <Card className="flex flex-row justify-center w-[50%] m-auto my-8" imgSrc="public/nodelogo.png" horizontal>
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+          </p>
+        </Card>
+      );
+    
+    }
+    
 
 export default PostCard
